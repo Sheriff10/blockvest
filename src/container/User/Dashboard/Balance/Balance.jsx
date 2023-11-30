fe0bf7e4f6227626319a5455b083a4bd76a32036
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-11-30 04:53:17 +0100

    added investments

--- src/container/User/Dashboard/Balance/Balance.jsx
+++ src/container/User/Dashboard/Balance/Balance.jsx
@@ -3,12 +3,13 @@ import Balance from "../../components/Balance";
 import Menu from "../../components/Menu";
 
 export default function UserBalance() {
+   const balance = window.sessionStorage.getItem("balance");
    return (
       <Menu>
          <div className="container">
-         <div className="bg-gray-900 rounded-xl my-16">
-            <Balance />
-         </div>
+            <div className="bg-gray-900 rounded-xl my-16">
+               <Balance balance={balance} />
+            </div>
          </div>
       </Menu>
    );
